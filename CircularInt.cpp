7b538ef54dfe03d9b68f8882269f5050eb6217b9
GropperTom <37331315+GropperTom@users.noreply.github.com>
2018-04-22 18:43:02 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,1 +1,331 @@
 #include "CircularInt.hpp"
+
+CircularInt::CircularInt(int min, int max) {}
+CircularInt::CircularInt() {}
+CircularInt CircularInt::operator=(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator+(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator+(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator-(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator-(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator-()
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator*(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator*(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator/(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator/(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator%(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator%(const int & other)
+{
+	return CircularInt();
+}
+CircularInt & CircularInt::operator+=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator+=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator-=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator-=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator*=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator*=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator/=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator/=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator%=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator%=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator++()
+{
+	return *this;
+}
+CircularInt CircularInt::operator++(int)
+{
+	return CircularInt();
+}
+CircularInt & CircularInt::operator--()
+{
+	return *this;
+}
+CircularInt CircularInt::operator--(int)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator&(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator&(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator|(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator|(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator^(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator^(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator~()
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator<<(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator<<(const int & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator>>(const CircularInt & other)
+{
+	return CircularInt();
+}
+CircularInt CircularInt::operator>>(const int & other)
+{
+	return CircularInt();
+}
+CircularInt & CircularInt::operator&=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator&=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator|=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator|=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator^=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator^=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator<<=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator<<=(const int & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator>>=(const CircularInt & other)
+{
+	return *this;
+}
+CircularInt & CircularInt::operator>>=(const int & other)
+{
+	return *this;
+}
+bool CircularInt::operator==(const CircularInt & other)
+{
+	return false;
+}
+bool CircularInt::operator==(const int & other)
+{
+	return false;
+}
+bool CircularInt::operator!=(const CircularInt & other)
+{
+	return false;
+}
+bool CircularInt::operator!=(const int & other)
+{
+	return false;
+}
+bool CircularInt::operator>(const CircularInt & other)
+{
+	return false;
+}
+bool CircularInt::operator>(const int & other)
+{
+	return false;
+}
+bool CircularInt::operator<(const CircularInt & other)
+{
+	return false;
+}
+bool CircularInt::operator<(const int & other)
+{
+	return false;
+}
+bool CircularInt::operator>=(const CircularInt & other)
+{
+	return false;
+}
+bool CircularInt::operator>=(const int & other)
+{
+	return false;
+}
+bool CircularInt::operator<=(const CircularInt & other)
+{
+	return false;
+}
+bool CircularInt::operator<=(const int & other)
+{
+	return false;
+}
+;
+
+CircularInt operator+(const int & a, CircularInt b)
+{
+	return b;
+}
+
+CircularInt operator-(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator*(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator/(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator%(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator&(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator|(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator^(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator<<(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+CircularInt operator>>(const int & a, CircularInt b)
+{
+	return CircularInt();
+}
+
+bool operator==(const int & a, CircularInt b)
+{
+	return false;
+}
+
+bool operator!=(const int & a, CircularInt b)
+{
+	return false;
+}
+
+bool operator>(const int & a, CircularInt b)
+{
+	return false;
+}
+
+bool operator<(const int & a, CircularInt b)
+{
+	return false;
+}
+
+bool operator>=(const int & a, CircularInt b)
+{
+	return false;
+}
+
+bool operator<=(const int & a, CircularInt b)
+{
+	return false;
+}
+
+ostream & operator<<(ostream & out, const CircularInt & cirInt)
+{
+	return out;
+}
+
+istream & operator>>(istream & in, const CircularInt & cirInt)
+{
+	return in;
+}
